926245402114ad049d19011f8cdc46f3c7f07e51
Eliezerproj2 <113759192+Eliezer2007z@users.noreply.github.com>
2025-04-25 15:04:09 -0300

    ok

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-
-
+document.addEventListener('DOMContentLoaded', function()(
 const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
 const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
 botaoDeAcessibilidade.addEventLister('click', function(){
-botaoDeAcessibilidade.ClassList.toggle('rotacao-botao');
-opcoesDeAcessibilidade.ClassList.toggle('apresenta-lista')
+    botaoDeAcessibilidade.ClassList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.ClassList.toggle('apresenta-lista')
+})
+
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+let tamanhoAtualFonte = 1;
+
+aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = '${tamanhoAtualFonte}ren'
+})
+    
+    diminuiFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = '$(tamanhoAtualFonte)ren'
+    
 })
